bb1c7430ace24036f85136e51dfb993856c5a06f
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-02-26 20:22:02 -0500

    created axios post request in submitForm()

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,40 @@
 console.log('script sourced.');
+
+function getQuotes() {
+    axios.get('/quotes').then((response) => {
+        // Code that will run on successful response
+        // from the server
+        console.log(response);
+        // quotesFromServer will be an Array of quotes
+        let quotesFromServer = response.data;
+        let contentDiv = document.getElementById('content');
+        contentDiv.innerHTML = '';
+        for (let quote of quotesFromServer) {
+            contentDiv.innerHTML += `
+                <p>"${quote.text}" - ${quote.author}</p>
+            `
+        }
+    }) 
+}
+
+getQuotes();
+
+let submitForm = event => {
+    event.preventDefault();
+    console.log(event);
+    let text = event.target[0].value;
+    let author = event.target[1].value;
+    console.log(text, author);
+    let quoteForServer = {
+        text,
+        author
+    };
+    axios.post('/quotes', quoteForServer).then((response) => {
+        console.log(response);
+        getQuotes();
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong.');
+    })
+};
+
--- server/server.js
+++ server/server.js
@@ -6,6 +6,38 @@ const app = express();
 // Use 5001 for localhost development
 const port = process.env.PORT || 5001;
 
+let quoteList = [
+    { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+//Allow req.body
+//ignore references to body-parser
+app.use(express.json());
+
+// GET request returns information po ()
+// localhost:5001/quotes
+// /quotes is the route
+app.get('/quotes', (req, res) => {
+    console.log('GET Request made for /quotes');
+    res.send(quoteList);
+    // res.send(quoteList[Math.floor(Math.random() * quoteList.length)]);
+    // Send back the list of quotes 
+});
+
+app.post('/quotes', (req, res) => {
+    console.log('post request made for /quotes');
+    console.log(req.body); //contains data sent from client
+    let quoteToAdd = req.body;
+    quoteList.push(quoteToAdd);
+    console.log(quoteList)
+    res.sendStatus(201); //means request was successful
+})
+// POST request saves user input
+// PUT request updates information
+// DELET request removes information
+
 // Look here for files
 app.use(express.static('server/public'));
 
